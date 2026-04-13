71713f182ff1a31781b6b7b537f3b2f704f75ed7
qrl512 <qrl512@york.ac.uk>
2026-04-13 12:41:24 +0100

    First attempt to get datasets
    First try to get the train and test datasets then resize and convert images so that my future neural network can handle the images. Needs testing

--- data.py
+++ data.py
@@ -1,1 +1,29 @@
-#handles dataset loading
+# must handle dataset loading of the Oxford Pets dataset
+
+import torch #core pytorch
+from torchvision import datasets, transforms #transforms is an image processing tool
+
+def get_datasets():
+    # .Compose transforms multiple steps into one pipeline so that they run in order, maybe look more into if this is a good choice
+    transform = transforms.Compose([
+        transforms.resize((128, 128)), #resize every image to 128x128 pixels so that all images are the same size, possibly adjust pixel size for resize if 128 isn't right choice
+        transforms.ToTensor() #convert image into a numeric arrary (tensor) so that the NN can understand it
+    ])
+
+    # Load Train Dataset
+    train_dataset = datasets.OxfordIIITPet(
+        root = "./data", #dataset is in folder 'data' in project, must remember to put datasets in data folder or think how to load them elsewhere
+        split = "trainval", #trainval = training + validation
+        transform = transform, #applies the processing pipeline from above
+        download = True #if no dataset on computer then just download automatically
+    )
+
+    # Load Test Dataset
+    test_dataset = datasets.OxfordIIITPet(
+        root = "./data",
+        split = "test",
+        transform = transform,
+        download = True
+    )
+
+    return train_dataset, test_dataset
