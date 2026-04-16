c2134c2397ff6cc1d02cf37a1e05d36b8e5bf14c
qrl512 <qrl512@york.ac.uk>
2026-04-16 15:21:34 +0100

    Neural Network Model Setup
    Setting up the animal classifier class inheriting from nn.Module (Pytorch base class) and mapping out the order the network will execute tasks. Some brainstorming comments left in for future self.

--- model.py
+++ model.py
@@ -1,1 +1,22 @@
 #CNN architecture code
+#Layers, activation functions (probably ReLU), forward pass, loss + backprop?
+#Pytorch models need to inherit from nn.Module (the base class)
+
+#imports
+import torch.nn as nn
+
+class AnimalClassifier(nn.Module):
+    #look through the nn.Module parent class to see the constructor
+    #must make init call to parent class before assignment of the child class -> pytorch base class info
+
+    def __init__(self):
+        #call the parent class constructor
+        super().__init__()
+
+        #my network layers
+
+    #parent class def forward -> this is data flow and forward pass
+    def forward(self, x):
+        #need to define x
+        #forward pass, data flow where i will connect layers and possibly pooling depending if the 128x128 image size is the right choice
+        
