d41fade7cfe1426ad03de28a8ae88dfd3e015b45
qrl512 <qrl512@york.ac.uk>
2026-04-16 15:28:14 +0100

    Added missing import + comments
    Mapped out the layers I need in comments and added a missing import of functional which has a bunch of neural network operations

--- model.py
+++ model.py
@@ -4,6 +4,7 @@
 
 #imports
 import torch.nn as nn
+import torch.nn.functional as F
 
 class AnimalClassifier(nn.Module):
     #look through the nn.Module parent class to see the constructor
@@ -19,4 +20,10 @@ class AnimalClassifier(nn.Module):
     def forward(self, x):
         #need to define x
         #forward pass, data flow where i will connect layers and possibly pooling depending if the 128x128 image size is the right choice
-        
+
+#LAYERS
+# Convolutional layers: extract features from the image like textures, shapes and edges
+# Pooling layer: reduce image size and see if it is good generalisation (model predicts correctly for data)
+# Flattening layer: convert image's features into 1d vector -> conv layers output 3d features and conneted layres need 1d vectors
+# Connected layers (full connected): perform the actual classification
+# Final output (not really a layer): output vector (shoulod be 37 numbers as each number corresponds to one animal breed (one class)
