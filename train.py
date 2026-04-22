b9ac049dfcc8f45973da95f4de6a52239e223fd7
qrl512 <qrl512@york.ac.uk>
2026-04-22 16:26:54 +0100

    Loss function and optimiser chosen
    1. Chose SGD but idk if I will use with momentum or not yet, think I need to do a bit more research into it to see if it is the right thing for the datasets
    2. Chose NLL for the loss function but I need to get the outputs in probability form so I've been looking at log softmax to see if that will work

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 #this must: load the train dataset, create the model, train for 30 epochs, save model to .pth file
 import torch.nn as nn
 from torch.utils.data import DataLoader
+from model import PetClassifier
 
 def train():
     #load the datatsets, look to see if i can make this better in the future inside data.py, low priority task though
@@ -11,14 +12,23 @@ def train():
 
     #initialise the model, loss and optimiser
     model = PetClassifier()
-    #loss?
-    #optimiser?
+    #loss? from letures probs NLL loss but need it for multiple classes, add softmax bc my outputs aren't probabilities which NLL needs not raw
+    loss_function = nn.NLLLoss() #add log softmax in model -> https://docs.pytorch.org/docs/stable/generated/torch.nn.LogSoftmax.html
+
+    #optimiser? lectures showen stochastic gradient descent but is that good enough? torch has SGD which is pretty helpful for me
+    optimiser = torch.optim.SGD(model.Parameters(), lr = 0.001, momentum = 0) #current values just the same as the Pytorch class definition, need to investigate whether i want momentum or not
+
+
 
     epochs = 30 #coursework doc specifies 30, make sure it stays like this
     for epoch in range(epochs):
         #loop through each epoch
         #training phase
+        model.train()
+        train_loss = 0 #accumulate over epochs
+        train_correct = 0 
 
+        
 
         #validation phase
 
